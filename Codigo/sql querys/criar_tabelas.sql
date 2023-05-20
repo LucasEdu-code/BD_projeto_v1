ebe5bf87c176c904c540f3a5208d136b45aac5ab
gabriel <gabrielsc.dev@gmail.com>
2023-05-20 18:00:18 -0300

    Código para criar as tabelas em um banco de dados postgres.

---
+++ Codigo/sql querys/criar_tabelas.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS mesa (
+	id_mesa serial,
+	numero_integrantes integer,
+	consumo_total float,
+	pago bool,
+	PRIMARY KEY (id_mesa)
+);
+
+CREATE TABLE IF NOT EXISTS prato (
+	id_prato serial,
+	preco float,
+	prato_nome text,
+	prato_categoria text,
+	prato_tipo text,
+	PRIMARY KEY (id_prato)
+);
+
+CREATE TABLE IF NOT EXISTS pedido (
+	id_pedido serial,
+	id_mesa integer,
+	id_prato integer,
+	quantidade integer,
+	entregue bool,
+	data timestamp,
+	PRIMARY KEY (id_pedido),
+	FOREIGN KEY (id_mesa) REFERENCES mesa(id_mesa) ON DELETE RESTRICT,
+	FOREIGN KEY (id_prato) REFERENCES prato(id_prato) ON DELETE RESTRICT
+);
